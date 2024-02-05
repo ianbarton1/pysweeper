7f1b2b3d36e3cdfd7ce7258f14c26f27f76bc4ac
Ian Barton <ian@ianbarton.uk>
2024-02-05 23:34:44 +0000

    2024-02-05

--- main.py
+++ main.py
@@ -2,24 +2,22 @@ from grid import Grid
 import tkinter as ui
 
 main_window = ui.Tk()
-
-# grid = Grid(10, 10, 20, main_window)
+grid = Grid(10, 10, 99, main_window, None, None)
+# grid = Grid(16, 16, 40, main_window, None, None)
+# grid = Grid(30, 16, 99, main_window, None, None)
 #first_guess_scenario https://minesweepergame.com/strategy/guessing.php
-# grid = Grid(7, 4, 0, main_window, "0000000011111000010000000000", "0000000000000011101111110111")
+# grid = Grid(7, 4, 0, main_window, "0000000011111000000000001000", "0000000000000011101111110111")
 #second_scenario https://minesweepergame.com/strategy/guessing.php
 # grid = Grid(5, 5, 0, main_window, "0000001010110100001000000", "0000000001001011110111111")
-
 
 #thirda 1 mine
 # grid = Grid(5, 5, 0, main_window, "0000001000001000000000000", "0011100111110111111111111")
 # third 2 mine
 # grid = Grid(5, 5, 0, main_window, "0100010000001000000000000", "0011100111110111111111111")
 #third 3 mine
-grid = Grid(5, 5, 0, main_window, "1100010000001000000000000", "0011100111110111111111111")
+# grid = Grid(5, 5, 0, main_window, "1100010000001000000000000", "0011100111110111111111111")
 
-#
-
+#pink squares
+# grid = Grid(8, 5, 0, main_window, "0000000000000000100100100000000000000000", "0000000000000000000000001111111100000000")
 
 main_window.mainloop()
-
-print(grid)
